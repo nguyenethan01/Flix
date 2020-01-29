25e7f344b50ebd7c4078ac01c4617ad9bda87a93
nguyenethan01 <nguyenethan01@gmail.com>
2020-01-28 22:32:22 -0800

    Unit 2

---
+++ Unit0/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Unit0
+//
+//  Created by Ethan  Nguyen on 1/28/20.
+//  Copyright © 2020 nguyenethan01. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
